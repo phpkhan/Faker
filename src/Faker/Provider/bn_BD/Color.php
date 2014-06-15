d85487a752c32c121f136ad57cc9f41e04f128f8
Md Ariful Haque Khan <neconest@gmail.com>
2014-06-15 22:40:51 +0600

    Added color for bn_BD locale

---
+++ src/Faker/Provider/bn_BD/Color.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Faker\Provider\ru_RU;
+
+class Color extends \Faker\Provider\Color
+{
+    protected static $safeColorNames = array(
+        'কালো', 'মরূন', 'সবুজ', 'জলপাই রং',
+        'বেগনি-লাল', 'লাইম', 'নীল', 'রূপালী',
+        'ধূসর', 'হলদে', 'সাদা'
+    );
+
+    protected static $allColorNames = array(
+        'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine',
+        'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond',
+        'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue',
+        'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue',
+        'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan',
+        'DarkGoldenRod', 'DarkGray', 'DarkGreen', 'DarkKhaki',
+        'DarkMagenta', 'DarkOliveGreen', 'Darkorange', 'DarkOrchid',
+        'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue',
+        'DarkSlateGray', 'DarkTurquoise', 'DarkViolet', 'DeepPink',
+        'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick',
+        'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite',
+        'Gold', 'GoldenRod', 'Gray', 'Green', 'GreenYellow', 'HoneyDew',
+        'HotPink', 'IndianRed ', 'Indigo ', 'Ivory', 'Khaki', 'Lavender',
+        'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral',
+        'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGreen', 'LightPink',
+        'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSteelBlue',
+        'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine',
+        'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue',
+        'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue',
+        'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive',
+        'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen',
+        'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum',
+        'PowderBlue', 'Purple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon',
+        'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+        'SlateGray', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato',
+        'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'
+    );
+}
